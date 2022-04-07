424533f32cf06ff26ff26e391385ad1af85d6c4d
LorenaL579 <lorena.lici579@gmail.com>
2022-04-07 16:17:46 +0200

    updated bridge with new ui and functionalities

--- src/App.js
+++ src/App.js
@@ -1,67 +1,93 @@
-import React from 'react';
+import React from "react";
 
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
-import initBridge from './components/bridge'
-import setupNetwork from './functions/setupnetwork'
-import Header from './components/header'
-import Footer from './components/footer'
+import initBridge from "./components/bridge";
+// import setupNetwork from "./functions/setupnetwork";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import EthLogo from "./assets/ethLogo.svg";
+import AvaxLogo from "./assets/avaxLogo.svg";
+import NotConnected from "./components/NotConnected";
+import Connected from "./components/Connected";
+import DoubleArrows from "./assets/arrows.svg";
+import ExchangeCircle from "./assets/exchangeCircle.svg";
 
-const Bridge = initBridge({ bridgeETH: window.bridge_eth, bridgeBSC: window.bridge_bsc , tokenETH: window.token_dyp_eth, tokenBSC: window.token_dyp_bsc })
+
+const Bridge = initBridge({
+  bridgeETH: window.bridge_eth,
+  bridgeBSC: window.bridge_bsc,
+  tokenETH: window.token_dyp_eth,
+  tokenBSC: window.token_dyp_bsc,
+});
 
 class App extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-        is_wallet_connected: false,
-        darkTheme: false
-    }
+      is_wallet_connected: false,
+      darkTheme: false,
+    };
   }
   toggleTheme = () => {
-    let darkTheme = !this.state.darkTheme
-    document.body.classList[darkTheme?'add':'remove']('dark')
-    this.setState({ darkTheme })
-  }
+    let darkTheme = !this.state.darkTheme;
+    document.body.classList[darkTheme ? "add" : "remove"]("dark");
+    this.setState({ darkTheme });
+  };
 
   handleConnection = async () => {
     try {
-      let is_wallet_connected = await window.connectWallet()
+      let is_wallet_connected = await window.connectWallet();
 
- 
-      this.setState({is_wallet_connected, coinbase: await window.web3.eth.getCoinbase()})
-      
+      this.setState({
+        is_wallet_connected,
+        coinbase: await window.web3.eth.getCoinbase(),
+      });
     } catch (e) {
-      window.alertify.error(String(e))
+      window.alertify.error(String(e));
     }
     //setupNetwork()
-  }
+  };
 
-render() {
-
-  if (!this.state.is_wallet_connected) {
-    return (<div className='App text-center'>
-      <Header darkTheme={this.state.darkTheme} toggleTheme={this.toggleTheme} />
-      <div className='container App-container' style={{maxWidth: '838px', minHeight: '512px'}}>
-        <div className='mt-5'>
-          <h3 className='mb-4'>Please connect wallet to use this dApp</h3>
-          <button onClick={this.handleConnection} style={{borderRadius: '6px'}} className='btn btn-primary pr-5 pl-5'>
-            CONNECT WALLET</button>
+  render() {
+    return (
+      <div className="App text-center">
+        <Header
+          darkTheme={this.state.darkTheme}
+          toggleTheme={this.toggleTheme}
+        />
+        <div className="container App-container">
+          <div className="mt-4">
+            <div className="container-fluid">
+            <div className="exchangeWrapper">
+              <h3 className="leftTitleWrapper mb-0">
+                <img src={ExchangeCircle} alt="circle" className="chainlogos"/>
+                Exchange Path
+              </h3>
+              <div className="rightTitleWrapper">
+                <h3 className="mb-0 blockchain">
+                  <img src={EthLogo} alt="ethLogo" className="chainlogos"/>
+                  ETH
+                </h3>
+                <img src={DoubleArrows} alt="arrows" />
+                <h3 className="mb-0 blockchain">
+                  <img src={AvaxLogo} alt="bscLogo" className="chainlogos" height={25}/>
+                  AVAX
+                </h3>
+              </div>
+            </div>
+            {this.state.is_wallet_connected === false ? (
+              <NotConnected handleConnection={this.handleConnection} />
+            ) : (
+              <Connected />
+            )}</div>
+            <Bridge />
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>);
+    );
   }
-  return (
-    <div className="App">      
-      <Header darkTheme={this.state.darkTheme} toggleTheme={this.toggleTheme} />
-      <div style={{minHeight: '550px'}} className="App-container">
-      	<Route exact path='/' render={props => <Bridge {...props} />} />
-      </div>
-      <Footer />
-    </div>
-  );
-}
 }
 
 export default App;
